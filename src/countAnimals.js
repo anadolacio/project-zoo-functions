2d3f7c4206b8c23874fef8625a98e50a53b96a89
anadolacio <anadolacio@hotmail.com>
2022-11-01 19:05:12 -0300

    resolução do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
+  if (animal === undefined) {
+    const newAnimals = {};
+    species.forEach((specie) => { newAnimals[specie.name] = specie.residents.length; });
+    return newAnimals;
+  }
+
+  if (animal.sex === undefined) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+
+  return species.find((element2) =>
+    element2.name === animal.specie).residents.filter((resident) =>
+    resident.sex === animal.sex).length;
 }
 
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
