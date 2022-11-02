c9f3c8a506b1313b7fcc396948d07791b563ca76
anadolacio <anadolacio@hotmail.com>
2022-11-01 21:26:32 -0300

    Resolução do requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const countChild = entrants.filter((entrant) => entrant.age < 18).length;
+  const countAdult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const countSenior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child: countChild, adult: countAdult, senior: countSenior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  const numberOfVisitants = countEntrants(entrants);
+  const priceChild = numberOfVisitants.child * prices.child;
+  const priceAdult = numberOfVisitants.adult * prices.adult;
+  const priceSenior = numberOfVisitants.senior * prices.senior;
+  return priceChild + priceAdult + priceSenior;
 }
 
+console.log(calculateEntry([]));
 module.exports = { calculateEntry, countEntrants };
