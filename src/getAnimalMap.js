83a173325b1c67067da7ea972fde4368e2df194c
anadolacio <anadolacio@hotmail.com>
2022-11-04 21:30:49 -0300

    melhoria no requisito 11

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,8 +1,17 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
-const NEanimals = species.filter((specie) => specie.location === 'NE').specie.name;
-console.log(NEanimals);
+// const { species } = data;
+
+// function getAnimalsFromNE() {
+//  const location = species.filter((specie) => specie.location === 'NE');
+//  const animalsNE = location.map((animal) => animal.name);
+//  const arrayAnimal = {};
+//  const animalsName = location.filter((element) => element.residents.map((resident) => resident.name));
+
+//  return animalsName;
+
+// }
+// console.log(getAnimalsFromNE());
 // function getAllAnimalsInformations() {
 //     const NEanimals = species.map((specie) => specie.location === 'NE').name;
 //     console.log(NEanimals);
@@ -22,22 +31,21 @@ console.log(NEanimals);
 // }
 
 // function getAnimalMap() {
-    // if (!parameter) {
-    //     return employees.map((employee) => ({ id: employee.id,
-    //       fullName: `${employee.firstName} ${employee.lastName}`,
-    //       species: species.filter((specie) => employee.responsibleFor.includes(specie.id))
-    //         .map((animal) => animal.name),
-    //       locations: species.filter((specie) => employee.responsibleFor.includes(specie.id))
-    //         .map((animal) => animal.location),
-    //     }));
-    //   }
-      // verificar se a propriedade name tem no objeto passado.
-    
-    //   if (Object.hasOwn(options, 'includesNames') && Object.hasOwn(options, 'sex')) {
-    //     return getInformations(options);
-    //   }
-      // verificar se a propriedade id tem no objeto passado.
-    //   return getAllAnimalsInformations();
+// if (!parameter) {
+//     return employees.map((employee) => ({ id: employee.id,
+//       fullName: `${employee.firstName} ${employee.lastName}`,
+//       species: species.filter((specie) => employee.responsibleFor.includes(specie.id))
+//         .map((animal) => animal.name),
+//       locations: species.filter((specie) => employee.responsibleFor.includes(specie.id))
+//         .map((animal) => animal.location),
+//     }));
+//   }
+// verificar se a propriedade name tem no objeto passado.
+//   if (Object.hasOwn(options, 'includesNames') && Object.hasOwn(options, 'sex')) {
+//     return getInformations(options);
+//   }
+// verificar se a propriedade id tem no objeto passado.
+//   return getAllAnimalsInformations();
 // }
 
 // console.log(getAnimalMap());
